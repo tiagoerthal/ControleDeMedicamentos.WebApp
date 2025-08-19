7cd520124a63db998d02634a7297930f2ec45911
Tiago <tiagoeweber@gmail.com>
2025-08-19 20:44:45 -0300

    Implementada expressão lambada para injeção de contexto de dados

--- ControleDeMedicamentos.WebApp/Program.cs
+++ ControleDeMedicamentos.WebApp/Program.cs
@@ -1,7 +1,4 @@
 using ControleDeMedicamentos.Infraestrutura.Arquivos.Compartilhado;
-using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloFornecedor;
-using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloPaciente;
-using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloMedicamento;
 using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloFuncionario;
 
 namespace ControleDeMedicamentos.WebApp;
@@ -13,18 +10,8 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Injeção de dependências criadas por nós
-
-
-        builder.Services.AddScoped(ConfigurarContextoDados);
-        builder.Services.AddScoped<RepositorioFornecedorEmArquivo>();
-        builder.Services.AddScoped<RepositorioPacienteEmArquivo>();          // Injeta uma instância do serviço por requisição (ação) HTTP, essa instância acompanha a requisição do cliente
-        builder.Services.AddScoped<RepositorioMedicamentoEmArquivo>();
-        builder.Services.AddScoped<RepositorioFuncionarioEmArquivo>();
-
-
-
-        //builder.Services.AddSingleton<RepositorioFuncionarioEmArquivo>();     // Injeta uma instância única do serviço globalmente
-        //builder.Services.AddTransient<RepositorioFuncionarioEmArquivo>();     // Injeta uma instância nova do serviço toda vez que houver uma dependência ao longo de uma requisição
+        builder.Services.AddScoped((_) => new ContextoDados(true));
+        builder.Services.AddScoped<RepositorioFuncionarioEmArquivo>();          // Injeta uma instância do serviço por requisição (ação) HTTP, essa instância acompanha a requisição do cliente
 
         // Injeção de dependências da Microsoft.
         builder.Services.AddControllersWithViews();
@@ -43,10 +30,5 @@ public class Program
             pattern: "{controller=Home}/{action=Index}/{id?}");
 
         app.Run();
-    }
-
-    private static ContextoDados ConfigurarContextoDados(IServiceProvider serviceProvider)
-    {
-        return new ContextoDados(true);
     }
 }
