78962b7e45f3c2a1a73660fbaa2c0b19b989f1c9
Tiago <tiagoeweber@gmail.com>
2025-08-21 19:21:37 -0300

    Injeção Dos Repositorios na Program

--- ControleDeMedicamentos.WebApp/Program.cs
+++ ControleDeMedicamentos.WebApp/Program.cs
@@ -1,5 +1,8 @@
 using ControleDeMedicamentos.Infraestrutura.Arquivos.Compartilhado;
+using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloFornecedor;
 using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloFuncionario;
+using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloMedicamento;
+using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloPaciente;
 using Serilog;
 using Serilog.Events;
 
@@ -13,7 +16,10 @@ public class Program
 
         // Injeção de dependências criadas por nós
         builder.Services.AddScoped((_) => new ContextoDados(true));
-        builder.Services.AddScoped<RepositorioFuncionarioEmArquivo>();          // Injeta uma instância do serviço por requisição (ação) HTTP, essa instância acompanha a requisição do cliente
+        builder.Services.AddScoped<RepositorioFornecedorEmArquivo>();
+        builder.Services.AddScoped<RepositorioFuncionarioEmArquivo>();
+        builder.Services.AddScoped<RepositorioMedicamentoEmArquivo>();
+        builder.Services.AddScoped<RepositorioPacienteEmArquivo>();
 
         var caminhoAppData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
