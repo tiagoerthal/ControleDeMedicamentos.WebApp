e14878d95c6d7d4381ec3596dee9fd75d58cafa1
Tiago <tiagoeweber@gmail.com>
2025-09-02 20:28:05 -0300

    Implementadas views e controller de Requisições de Entrada

---
+++ ControleDeMedicamentos.WebApp/Controllers/RequisicaoMedicamentoController.cs
@@ -0,0 +1,89 @@
+﻿using ControleDeMedicamentos.Dominio.ModuloRequisicaoMedicamento;
+using ControleDeMedicamentos.Infraestrutura.Arquivos.Compartilhado;
+using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloFuncionario;
+using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloMedicamento;
+using ControleDeMedicamentos.Infraestrutura.Arquivos.ModuloRequisicaoMedicamento;
+using ControleDeMedicamentos.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ControleDeMedicamentos.WebApp.Controllers;
+
+public class RequisicaoMedicamentoController : Controller
+{
+    private readonly ContextoDados contexto;
+    private readonly RepositorioRequisicaoMedicamentoEmArquivo repositorioRequisicaoMedicamento;
+    private readonly RepositorioMedicamentoEmArquivo repositorioMedicamento;
+    private readonly RepositorioFuncionarioEmArquivo repositorioFuncionario;
+
+    public RequisicaoMedicamentoController(
+        ContextoDados contexto,
+        RepositorioRequisicaoMedicamentoEmArquivo repositorioRequisicaoMedicamento,
+        RepositorioMedicamentoEmArquivo repositorioMedicamento,
+        RepositorioFuncionarioEmArquivo repositorioFuncionario
+    )
+    {
+        this.contexto = contexto;
+        this.repositorioRequisicaoMedicamento = repositorioRequisicaoMedicamento;
+        this.repositorioMedicamento = repositorioMedicamento;
+        this.repositorioFuncionario = repositorioFuncionario;
+    }
+
+    [HttpGet]
+    public IActionResult Index()
+    {
+        var requisicoesEntrada = repositorioRequisicaoMedicamento.SelecionarRequisicoesEntrada();
+
+        var visualizarVm = new VisualizarRequisicoesMedicamentoViewModel(requisicoesEntrada);
+
+        return View(visualizarVm);
+    }
+
+    [HttpGet]
+    public IActionResult CadastrarRequisicaoEntrada()
+    {
+        var medicamentosDisponiveis = repositorioMedicamento.SelecionarRegistros();
+        var funcionariosDisponiveis = repositorioFuncionario.SelecionarRegistros();
+
+        var cadastrarVm = new CadastrarRequisicaoEntradaViewModel(medicamentosDisponiveis, funcionariosDisponiveis);
+
+        return View(cadastrarVm);
+    }
+
+    [HttpPost]
+    public IActionResult CadastrarRequisicaoEntrada(CadastrarRequisicaoEntradaViewModel cadastrarVm)
+    {
+        if (!ModelState.IsValid)
+        {
+            var medicamentosDisponiveis = repositorioMedicamento.SelecionarRegistros();
+
+            cadastrarVm.MedicamentosDisponiveis = medicamentosDisponiveis
+                .Select(m => new SelectListItem(m.Nome, m.Id.ToString()))
+                .ToList();
+
+            var funcionariosDisponiveis = repositorioFuncionario.SelecionarRegistros();
+
+            cadastrarVm.FuncionariosDisponiveis = funcionariosDisponiveis
+                .Select(f => new SelectListItem(f.Nome, f.Id.ToString()))
+                .ToList();
+
+            return View(cadastrarVm);
+        }
+
+        var funcionarioSelecionado = repositorioFuncionario.SelecionarRegistroPorId(cadastrarVm.FuncionarioId);
+
+        var medicamentoSelecionado = repositorioMedicamento.SelecionarRegistroPorId(cadastrarVm.MedicamentoId);
+
+        var requisicaoEntrada = new RequisicaoEntrada(
+            funcionarioSelecionado,
+            medicamentoSelecionado,
+            cadastrarVm.QuantidadeRequisitada
+        );
+
+        medicamentoSelecionado.AdicionarAoEstoque(requisicaoEntrada);
+
+        repositorioRequisicaoMedicamento.CadastrarRequisicaoEntrada(requisicaoEntrada);
+
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ ControleDeMedicamentos.WebApp/Models/RequisicaoMedicamentoViewModel.cs
@@ -0,0 +1,77 @@
+﻿using ControleDeMedicamentos.Dominio.ModuloFuncionario;
+using ControleDeMedicamentos.Dominio.ModuloMedicamento;
+using ControleDeMedicamentos.Dominio.ModuloRequisicaoMedicamento;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeMedicamentos.WebApp.Models;
+
+public class CadastrarRequisicaoEntradaViewModel
+{
+    [Required(ErrorMessage = "O campo 'Medicamento' é obrigatório.")]
+    public Guid MedicamentoId { get; set; }
+    public List<SelectListItem>? MedicamentosDisponiveis { get; set; }
+
+    [Required(ErrorMessage = "O campo 'Funcionário' é obrigatório.")]
+    public Guid FuncionarioId { get; set; }
+    public List<SelectListItem>? FuncionariosDisponiveis { get; set; }
+
+    [Required(ErrorMessage = "O campo 'Quantidade Requisitada' é obrigatório.")]
+    [Range(0, int.MaxValue, ErrorMessage = "O campo 'Quantidade Requisitada' deve conter um número positivo válido.")]
+    public int QuantidadeRequisitada { get; set; }
+
+    public CadastrarRequisicaoEntradaViewModel() { }
+
+    public CadastrarRequisicaoEntradaViewModel(List<Medicamento> medicamentos, List<Funcionario> funcionarios)
+    {
+        MedicamentosDisponiveis = medicamentos
+            .Select(m => new SelectListItem(m.Nome, m.Id.ToString()))
+            .ToList();
+
+        FuncionariosDisponiveis = funcionarios
+            .Select(f => new SelectListItem(f.Nome, f.Id.ToString()))
+            .ToList();
+    }
+}
+
+public class VisualizarRequisicoesMedicamentoViewModel
+{
+    public List<DetalhesRequisicaoEntradaViewModel> RequisicoesEntrada { get; set; }
+
+    public VisualizarRequisicoesMedicamentoViewModel(List<RequisicaoEntrada> requisicoesEntrada)
+    {
+        RequisicoesEntrada = requisicoesEntrada
+            .Select(r => new DetalhesRequisicaoEntradaViewModel(
+                r.Id,
+                r.DataOcorrencia,
+                r.Funcionario.Nome,
+                r.Medicamento.Nome,
+                r.QuantidadeRequisitada
+            ))
+            .ToList();
+    }
+}
+
+public class DetalhesRequisicaoEntradaViewModel
+{
+    public Guid Id { get; set; }
+    public DateTime DataOcorrencia { get; set; }
+    public string Funcionario { get; set; }
+    public string Medicamento { get; set; }
+    public int QuantidadeRequisitada { get; set; }
+
+    public DetalhesRequisicaoEntradaViewModel(
+        Guid id,
+        DateTime dataOcorrencia,
+        string funcionario,
+        string medicamento,
+        int quantidadeRequisitada
+    )
+    {
+        Id = id;
+        DataOcorrencia = dataOcorrencia;
+        Funcionario = funcionario;
+        Medicamento = medicamento;
+        QuantidadeRequisitada = quantidadeRequisitada;
+    }
+}
